f5b493710975881d1033e3eb3b54eb0ba90e9ed8
Sara3Dam1 <sara.cavalcanti@escola.pr.gov.br>
2026-06-29 13:14:07 +0000

    First commit

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1', 
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'loja'
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log('Conectado ao MySQL!');
+});
+
+module.exports = connection;
